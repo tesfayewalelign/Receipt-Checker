c8a110b5fc12608b076df601ac55730d463b5e58
Abu2013 <tesfayewalelign44@gmail.com>
2026-04-23 14:06:44 -0400

    feat(auth): complete Better Auth setup and integrate backend authentication foundation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import verificationRoutes from "./modules/verification/routes/index";
 import { errorHandler } from "./middlewares/error.middleware";
+import { toNodeHandler } from "better-auth/node";
+import auth from "./lib/auth";
 
 dotenv.config();
 
 const app = express();
-
+app.all("/api/auth/*", toNodeHandler(auth));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
---
+++ backend/src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+});
+
+export default auth;
